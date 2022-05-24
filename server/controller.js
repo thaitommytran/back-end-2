3554d81936627e61656b387c28bfb24ec7798d96
Thai Tran <thai.tommy.tran@gmail.com>
2022-05-24 14:58:35 -0500

    back end 2 completed

--- server/controller.js
+++ server/controller.js
@@ -1,3 +1,44 @@
+const houses = require("./db.json");
+
+let globalId = 4;
+
 module.exports = {
-    getHouses: () = 
-}
+  getHouses: (req, res) => {
+    res.status(200).send(houses);
+  },
+  deleteHouse: (req, res) => {
+    let index = houses.findIndex((element) => element.id === +req.params.id);
+    houses.splice(index, 1);
+    res.status(200).send(houses);
+  },
+  createHouse: (req, res) => {
+    let { address, price, imageURL } = req.body;
+
+    let newHouse = {
+      id: globalId,
+      address,
+      price,
+      imageURL
+    };
+
+    houses.push(newHouse);
+    res.status(200).send(houses);
+    globalId++;
+  },
+  updateHouse: (req, res) => {
+    let id = req.params.id;
+    let type = req.body.type;
+    let index = houses.findIndex((element) => element.id === +id);
+
+    if (type === "minus") {
+      houses[index].price -= 10000;
+      if (houses[index].price < 0) {
+        houses[index].price = 0;
+      }
+      res.status(200).send(houses);
+    } else if (type === "plus") {
+      houses[index].price += 10000;
+      res.status(200).send(houses);
+    }
+  }
+};
--- server/index.js
+++ server/index.js
@@ -8,4 +8,9 @@ const controllerFile = require("./controller");
 app.use(express.json());
 app.use(cors());
 
+app.get("/api/houses", controllerFile.getHouses);
+app.post("/api/houses", controllerFile.createHouse);
+app.put("/api/houses/:id", controllerFile.updateHouse);
+app.delete("/api/houses/:id", controllerFile.deleteHouse);
+
 app.listen(4004, () => console.log(`running on 4004`));
